d0167bf5955d79faeb773e150b5456ddfc5304a0
code-lucky <2924249993@qq.com>
2024-07-10 23:11:09 +0800

    update

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,47 +1,83 @@
 'use client';
-import React, { useState } from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
-import Menus from './components/menus'
-import './css/header.css'
+import React, { useState } from "react";
+import { Navbar, NavbarBrand, NavbarMenuToggle, NavbarMenuItem, NavbarMenu, NavbarContent, NavbarItem, Link, Button, Dropdown, DropdownTrigger, DropdownMenu, DropdownItem } from "@nextui-org/react";
+import Image from "next/image";
+import './css/Header.css';
 import { defaultLocale } from '@/lib/i18n'
 
-// menu list
-const menus = [
-  { name: 'Feature', link: '/#feature' },
-  { name: 'Pricing', link: '/#pricing' },
-  { name: 'FAQ', link: '/#faq' },
-]
-
-export default function Header() {
-
+export default function App() {
   const [langName, setLangName] = useState(defaultLocale);
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+  const menus = [
+    { name: 'Feature', link: '/#feature' },
+    { name: 'Pricing', link: '/#pricing' },
+    { name: 'FAQ', link: '/#faq' },
+  ]
 
   return (
-    <>
-      <header className='flex justify-center px-5 border-b'>
-        <section className="max-w-screen-xl w-full flex h-14 items-center md:gap-28 sm:gap-10 gap-5 justify-between">
-          <Link href={langName}>
-            <Image src="vercel.svg" alt="Logo" width={100} height={100} />
+    <Navbar
+      isBordered
+      isMenuOpen={isMenuOpen}
+      onMenuOpenChange={setIsMenuOpen}
+      maxWidth="xl"
+    >
+      <NavbarContent className="sm:hidden pr-3" justify="center">
+        <Link href="/">
+          <Image src="/vercel.svg" alt="ACME" width={100} height={100} />
+        </Link>
+      </NavbarContent>
+
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        <NavbarBrand className="mr-20">
+          <Link href="/">
+            <Image src="/vercel.svg" alt="ACME" width={100} height={100} />
           </Link>
+        </NavbarBrand>
+        <div className="flex gap-20">
+          {menus.map((item, index) => (
+            <NavbarItem key={`${item}-${index}`}>
+              <Link color="foreground" href={`${langName}/${item.link}`} className="menu-link">
+                {item.name}
+              </Link>
+            </NavbarItem>
+          ))}
+        </div>
+      </NavbarContent>
 
-          <div className='hidden md:flex flex-1 justify-between'>
-            {/* 鼠标hover出现下划线 */}
-            <menu className='flex md:gap-20 sm:gap-5 gap-3'>
-              {menus.map((menu, index) => (
-                <Link href={`${langName}/${menu.link}`} key={index} className='menu-link'>{menu.name}</Link>
-              ))}
-            </menu>
-          </div>
+      <NavbarContent justify="end">
+        <Dropdown >
+          <DropdownTrigger>
+            <Button
+              variant="light"
+            >
+              EN
+            </Button>
+          </DropdownTrigger>
+          <DropdownMenu aria-label="Example with disabled actions">
+            <DropdownItem key="new">New file</DropdownItem>
+            <DropdownItem key="copy">Copy link</DropdownItem>
+            <DropdownItem key="edit">Edit file</DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+        <NavbarMenuToggle className="sm:hidden" aria-label={isMenuOpen ? "Close menu" : "Open menu"} />
+      </NavbarContent>
 
-          <div>
-            <div>zh</div>
-            <div className='flex md:hidden'>
-              <Menus linkList={menus}/>
-            </div>
-          </div>
-        </section>
-      </header>
-    </>
-  )
+      <NavbarMenu>
+        {menus.map((item, index) => (
+          <NavbarMenuItem key={`${item}-${index}`}>
+            <Link
+              className="w-full"
+              color="foreground"
+              href={`${langName}/${item.link}`}
+              size="lg"
+              onClick={() => setIsMenuOpen(false)}
+            >
+              {item.name}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
+    </Navbar>
+  );
 }
